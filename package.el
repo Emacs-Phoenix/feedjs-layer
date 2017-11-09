1f9e6555e84dbea1c562d1b242d29e4c47bda22a
fwchen <crystal.tyan@gmail.com>
2017-11-09 23:09:45 +0800

    feedjs-layer initial commit

---
+++ package.el
@@ -0,0 +1,14 @@
+;;; packages.el --- feedjs Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Fangwei chen
+;;
+;; Author: Fangwei Chen <chenfangwei@outlook.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst feedjs-packages
+  '(
+    emacs-feedjs
+    ))
