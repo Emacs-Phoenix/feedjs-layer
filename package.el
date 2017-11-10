5bfb7915ab5602764a24b05eca0144bbaf27932c
fwchen <crystal.tyan@gmail.com>
2017-11-10 22:31:35 +0800

    initial copy

--- package.el
+++ package.el
@@ -12,3 +12,33 @@
   '(
     emacs-feedjs
     ))
+
+(defun feedjs/init-slack ()
+  "Initialize Slack"
+  (use-package emacs-feedjs
+    :commands (feedjs)
+    :defer t
+    :init
+    (progn
+      (spacemacs/set-leader-keys
+        "aCs" 'slack-start
+        "aCj" 'slack-channel-select
+        "aCd" 'slack-im-select
+        "aCq" 'slack-ws-close)
+      (setq slack-enable-emoji t))
+    :config
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'feedjs-mode
+        "j" 'slack-channel-select
+        "d" 'slack-im-select
+        "p" 'slack-room-load-prev-messages
+        "e" 'slack-message-edit
+        "q" 'slack-ws-close
+        "mm" 'slack-message-embed-mention
+        "mc" 'slack-message-embed-channel
+        "k" 'slack-channel-select
+        "@" 'slack-message-embed-mention
+        "#" 'slack-message-embed-channel)
+      (evil-define-key 'insert slack-mode-map
+        (kbd "@") 'slack-message-embed-mention
+        (kbd "#") 'slack-message-embed-channel))))
